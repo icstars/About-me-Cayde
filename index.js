7cf70a3d2d6b673c85e3aa2404df7405f1c3c1db
@CyberCayde <cfroemming@icstars.org>
2023-11-06 15:04:11 -0600

    Script Pushed
    ...

---
+++ index.js
@@ -0,0 +1,68 @@
+// VAR, LET, CONST
+
+let x = 0 // mutable/changeabl variable
+
+x = 1
+
+console.log(x)
+
+{
+    var i =0
+}
+
+console.log(i)
+
+//DATA TYPES
+
+//NUMBER/INTEGER
+let Number = 5
+
+console.log(number+10)
+
+number = number + 1
+
+//STRING
+
+letname = "Tao"
+// concatenation
+name = name + 'Cha'
+console.log(name)
+
+//BOOLEAN
+//true or false 
+
+//TRUTHY
+1, 'Tao',true
+
+//FALSY
+0, null, undefined, '', false
+
+??  if('')
+
+//ARRAY
+
+let arr = [1,2,3,4,5]
+
+//OBJECTS
+
+const vince ={
+    age: 21
+    hobby: 'dadadadadada'
+    email: 'vince@hotmail.com'
+    pw: 123
+}
+
+//LOOPS
+
+let profiles = 
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+//FUNCTIONS
+
+function test(name) {
+    console.log(name)
+}
+test
